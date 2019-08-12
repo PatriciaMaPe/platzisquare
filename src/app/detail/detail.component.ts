f8c10765cf117c3251a0c99562acb70e44db0c1f
pmperez <pmperez@esei.uvigo.es>
2019-08-12 20:32:21 +0200

    Show the details of the company selected

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -6,10 +6,30 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './detail.component.html'
 })
 export class DetailComponent {
+  places:any = [
+    {id: 1, plan: 'premium', proximity:1, distance: 1, active:true, name:'Flower shop Gardenia', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 2, plan: 'free', proximity:1, distance: 1.8, active:false, name:'Bakery Dona', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 3, plan: 'free', proximity:2, distance: 10, active:true, name:'Veterinary Little paws', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 4, plan: 'premium', proximity:3, distance: 35, active:true, name:'Takako Sushi Restaurant', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 5, plan: 'free', proximity:3, distance: 120, active:true, name:'PizzaSpot', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'}
+  ];
+
+  id = null;
+  place:any = {};
+
   constructor(private route: ActivatedRoute){
     console.log(this.route.snapshot.params['id']);
     console.log(this.route.snapshot.queryParams['action']);
     console.log(this.route.snapshot.queryParams['referer']);
+    this.id = this.route.snapshot.params['id'];
+    this.place = this.getPlace();
+  }
+
+  getPlace(){
+    return this.places.filter( (place) => { return place.id == this.id } )[0] || null;
+    // like a foreach of the elements with the same id condition.
+    // Filter always return an array so we take the first element and
+    // if the condition isn't true, return null
   }
 
 }
--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -9,11 +9,11 @@ import { Component } from '@angular/core';
 export class PlacesComponent {
   title = 'PlatziSquare';
   places:any = [
-    {id: 1, plan: 'premium', proximity:1, distance: 1, active:true, name:'Flower shop Gardenia'},
-    {id: 2, plan: 'free', proximity:1, distance: 1.8, active:false, name:'Bakery Dona'},
-    {id: 3, plan: 'free', proximity:2, distance: 10, active:true, name:'Veterinary Little paws'},
-    {id: 4, plan: 'premium', proximity:3, distance: 35, active:true, name:'Takako Sushi Restaurant'},
-    {id: 5, plan: 'free', proximity:3, distance: 120, active:true, name:'PizzaSpot'}
+    {id: 1, plan: 'premium', proximity:1, distance: 1, active:true, name:'Flower shop Gardenia', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 2, plan: 'free', proximity:1, distance: 1.8, active:false, name:'Bakery Dona', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 3, plan: 'free', proximity:2, distance: 10, active:true, name:'Veterinary Little paws', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 4, plan: 'premium', proximity:3, distance: 35, active:true, name:'Takako Sushi Restaurant', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'},
+    {id: 5, plan: 'free', proximity:3, distance: 120, active:true, name:'PizzaSpot', description:'Lorem ipsum dolor sit amet consectetur adipiscing, elit magna aenean fringilla a platea, aliquet dictumst vulputate scelerisque orci.'}
   ];
   lat:number = 42.183513;
   lng:number = -8.8011916;
